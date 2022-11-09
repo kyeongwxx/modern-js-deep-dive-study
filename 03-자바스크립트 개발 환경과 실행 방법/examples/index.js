55e6e9bc819d8f59da23f5401fec034bb8c1270d
kimjaebeom1 <gojn0214@gmail.com>
2022-11-09 18:55:40 +0900

    3장 & 4장 정리 및 공부

---
+++ 03-자바스크립트 개발 환경과 실행 방법/examples/index.js
@@ -0,0 +1,13 @@
+// 예제 03-02
+
+const arr = [1, 2, 3];
+
+arr.forEach(console.log);
+
+// 예제 03-03
+
+// const arr1 = [1, 2, 3];
+
+// arr1.forEach(alert);
+
+// alert는 클라이언트 사이트 Web API 함수이기 때문에 node.js환경에서는 실행이 안되고 에러가 발생한다.
---
+++ 04-변수/examples/예제 04-03.js
@@ -0,0 +1,3 @@
+var score; // 변수 선언(변수 선언문)
+
+console.log(score);
