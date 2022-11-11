996f323429854ff45b8d0953472706594dcaf7c9
kyeongwxx <kyeongwxx@gmail.com>
2022-11-11 12:56:04 +0900

    05. 표현식과 문

---
+++ 05-표현식과 문/examples/05-09.js
@@ -0,0 +1,18 @@
+// 변수 선언문
+var x;
+
+// 할당문
+x = 5;
+
+// 함수 선언문
+function foo() {}
+
+// 조건문
+if (x > 1) {
+  console.log(x);
+}
+
+// 반복문
+for (let i = 0; i < array.length; i++) {
+  console.log(i);
+}
